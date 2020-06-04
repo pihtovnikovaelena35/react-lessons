032ec2dba3904086d4f9f8144b2d822e0b65c13c
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2020-06-04 18:43:57 +0300

    page demarcation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='app-wrapper'>
+    <Header />
+  <nav className='nav'>
+    <div>
+    <div>
+      <a>Profile</a>
+      </div>
+      <a>Messages</a>
+      </div>
+      <div>
+      <a>News</a>
+      <div>
+      <a>Music</a>
+      </div>
+      <div>
+      <a>Settings</a>
+      </div>
+      
+      </div>
+  </nav>
+  <div className='content'>
+    <div>
+    <img src='https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/denman20200325-nasa.jpg'/>
+ </div >
+ <div>
+   ava+description
+ </div>
+ <div>
+   My posts
+   <div>
+     New post
+   </div>
+   <div>
+   <div>
+     post1
+   </div>
+   <div>
+     post2
+   </div>
+   </div>
+ </div>
+  </div>
+</div>
+);
 }
+
 
 export default App;
