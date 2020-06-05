d52bb0d90b0c6a0feab0e3189295937d701d8764
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2020-06-05 18:19:05 +0300

    created a dialog box

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
     <Header />
     <Navbar />
-    <Profile />
+    <div className='app-wrapper-content'>
+    <Dialogs />
+    </div>
+    {/*<Profile />*/}
+
+ 
  </div>
 );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return  (
-     <div className={s.content}>
+     <div>
     <div>
     <img src='https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/denman20200325-nasa.jpg'/>
  </div >
