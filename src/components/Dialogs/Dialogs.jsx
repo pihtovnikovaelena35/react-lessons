a11a0e3fcfc84c323ddc30885b5bca56fee79980
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2020-06-06 16:53:38 +0300

    start dialog box

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import s from './Dialogs.module.css';
 const Dialogs = () => {
  return (
-     <div>
-         Dialogs
+     <div className={s.dialogs}>
+         <div className={s.dialogsItems}>
+             <div className={s.dialog + ' ' + s.active}>
+                 Sasha
+             </div>
+             <div className={s.dialog}>
+                Sveta
+             </div>
+             <div className={s.dialog}>
+                 Misha
+             </div>
+         </div>
+         <div className={s.messages}>
+         <div className={s.messag}>Hi</div>
+         <div className={s.messag}>Yo</div>
+         <div className={s.messag}>Hi</div>
+     </div>
      </div>
  );
 
