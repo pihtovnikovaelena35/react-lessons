b9a7b185e74e019877326ac3f2828a17d9662f75
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2020-06-06 14:59:29 +0300

    connected path

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
-  return (
+  return ( <BrowserRouter>
     <div className='app-wrapper'>
     <Header />
     <Navbar />
     <div className='app-wrapper-content'>
-    <Dialogs />
+      <Route path='/dialogs' component={Dialogs}/>
+      <Route path='/profile' component={Profile}/>
     </div>
-    {/*<Profile />*/}
-
- 
  </div>
+</BrowserRouter>
 );
 }
 
