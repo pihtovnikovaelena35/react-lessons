bb6dbd5ada685c73832d42b11ebb9f77750c4597
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2020-06-07 10:32:22 +0300

    created a link for the dialog box

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
 const Dialogs = () => {
  return (
      <div className={s.dialogs}>
          <div className={s.dialogsItems}>
              <div className={s.dialog + ' ' + s.active}>
-                 Sasha
+                 <NavLink to="/dialogs/1">Sasha</NavLink>
              </div>
              <div className={s.dialog}>
-                Sveta
+             <NavLink to="/dialogs/2">Sveta</NavLink>
              </div>
              <div className={s.dialog}>
-                 Misha
+             <NavLink to="/dialogs/3">Misha</NavLink>
+             </div>
+             <div className={s.dialog}>
+             <NavLink to="/dialogs/4">Masha</NavLink>
              </div>
          </div>
          <div className={s.messages}>
          <div className={s.messag}>Hi</div>
          <div className={s.messag}>Yo</div>
          <div className={s.messag}>Hi</div>
+          </div>
      </div>
-     </div>
- );
-
+ )
     
 }
 
